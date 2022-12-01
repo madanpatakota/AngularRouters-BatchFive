6b07b0f4e7999db194df6472a6871ee20c939cc7
MadanMohanReddy <madan.patakota@hotmail.com>
2022-12-01 08:37:43 +0530

    Angular Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SchoolsComponent } from './schools/schools.component';
+import { SchoolDetailsComponent } from './school-details/school-details.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+// http://localhost:4200/Main   ---> Maincomponent
+// http://localhost:4200/Schools   ---> Schoolscomponent
+// http://localhost:4200/SchoolDetails   ---> SchoolDetailscomponent
+
+// real time project
+// https://github.com/explore   -- Explore page
+// https://github.com/issues    -- issue page
+// https://github.com/codespaces  -- codespaces
+
+// Main   MainComponent
+// Schools SchoolsComponent
+
+const AppRoutes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'schools', component: SchoolsComponent },
+  { path: 'schooldetails', component: SchoolDetailsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SchoolsComponent,
+    SchoolDetailsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(AppRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
